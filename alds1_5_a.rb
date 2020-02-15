54b16e8fd14aa95490479c05832322ee627d1240
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-15 11:46:23 +0900

    solved alds1_5_a by bit brute force

--- alds1_5_a.rb
+++ alds1_5_a.rb
@@ -1,20 +1,20 @@
-def dfs(n, a, i, sum)
-  @h[sum] = true
-
-  return if i == n
-
-  dfs(n, a, i + 1, sum)
-  dfs(n, a, i + 1, sum + a[i])
-end
-
 n = gets.chomp.to_i
 a = gets.chomp.split.map(&:to_i)
 q = gets.chomp.to_i
 m = gets.chomp.split.map(&:to_i)
 
-@h = Hash.new(false)
-dfs(n, a, 0, 0)
+h = {}
+
+0.upto((1 << n) - 1) do |i|
+  sum = 0
+  0.upto(n - 1) do |j|
+    if i & (1 << j) > 0
+      sum += a[j]
+    end
+  end
+  h[sum] = true
+end
 
 m.each do |mi|
-  puts @h[mi] ? 'yes' : 'no'
+  puts h[mi] ? 'yes' : 'no'
 end
