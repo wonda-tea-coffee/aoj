5b6fec9d4670cc5fdc2c0ea15d510fe65ba8248a
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-01 08:12:52 +0900

    通るはず

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -6,75 +6,78 @@
 #include <cstring>
 #include <functional>
 #include <iostream>
+#include <iomanip>
+#include <limits>
 #include <map>
 #include <queue>
 #include <set>
 #include <stack>
 #include <string>
+#include <regex>
 #include <vector>
 
-#define rep(i,n)   for (int i = 0; i < (n); ++i)
-#define sort(a)    sort((a).begin(), (a).end());
-#define uniq(a)    SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end());
-#define find(a,x)  find((a).begin(), (a).end(), (x)) != (a).end()
-#define reverse(a) reverse((a).begin(), (a).end());
-#define out(d) cout << (d);
-#define outl(d) std::cout<<(d)<<"\n";
-#define Yes() printf("Yes\n");
-#define No() printf("No\n");
-#define YES() printf("YES\n");
-#define NO() printf("NO\n");
-#define ceil(x, y) ((x + y - 1) / (y))
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
 
 using namespace std;
 using ll = long long;
+using ld = long double;
 using P = pair<ll,ll>;
 
 const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
 
-const int MAX_N = 100;
-int d[MAX_N + 1];
-int N;
-vector<vector<int>> g(MAX_N + 1);
+ll n, k;
+vector<ll> x;
 
-void bfs() {
-  queue<P> q;
-  q.push(make_pair(1, 0));
+ll solve2() {
+  vector<ll> y(n-1);
+  for (int i = 1; i < n; i++)
+    y[i-1] = x[i] - x[i-1];
+  sort(y);
 
-  while (!q.empty()) {
-    P p = q.front(); q.pop();
-    int node = p.first;
-    int dist = p.second;
+  ll ans = x[n-1] - x[0];
+  for (int i = 0; i < k-1; i++)
+    ans -= y[n-2-i];
+  return max(ans, 0ll);
+}
 
-    if (d[node] > 0) continue;
-
-    d[node] = dist;
-    for (int i = 0; i < g[node].size(); i++) {
-      q.push(make_pair(g[node][i], dist + 1));
-    }
+void solve() {
+  ll t; cin >> t;
+  rep(i, t) {
+    cin >> n >> k;
+    x.resize(n);
+    rep(j, n) cin >> x[j];
+    outl(solve2());
   }
 }
 
-int main() {
+signed main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
   srand((unsigned)time(NULL));
+  fix(12);
 
-  cin >> N;
-  for (int i = 0; i < N; i++) {
-    int u; cin >> u;
-    int k; cin >> k;
-    for (int ki = 0; ki < k; ki++) {
-      int tmp; cin >> tmp;
-      g[u].push_back(tmp);
-    }
-    sort(g[u])
-  }
-  memset(d, 0, sizeof(d));
-
-  bfs();
-
-  for (int i = 1; i <= N; i++) {
-    cout << i << " " << d[i] << endl;
-  }
+  solve();
 }
