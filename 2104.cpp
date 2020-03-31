9a55af07c9caf060aa03d4aaf7d36c0814f367a9
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-31 21:51:37 +0900

    解説AC

--- 2104.cpp
+++ 2104.cpp
@@ -38,55 +38,18 @@
 #define fst             first
 #define int             long long
 #define INF             __LONG_LONG_MAX__
-#define MAX             200000
-#define SENTINEL        2000000000
 
 using namespace std;
 using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
 
-int L[MAX / 2 + 2], R[MAX / 2 + 2];
-
-ll merge(int A[], int n, int left, int mid, int right) {
-  ll i, j, k;
-  ll cnt = 0;
-  ll n1 = mid - left;
-  ll n2 = right - mid;
-  for (i = 0; i < n1; i++) L[i] = A[left + i];
-  for (i = 0; i < n2; i++) R[i] = A[mid + i];
-  L[n1] = R[n2] = SENTINEL;
-  i = j = 0;
-  for (k = left; k < right; k++) {
-    if (L[i] <= R[j]) {
-      A[k] = L[i++];
-    } else {
-      A[k] = R[j++];
-      cnt += n1 - i; // = mid + j - k - 1
-    }
-  }
-  return cnt;
-}
-
-ll mergeSort(int A[], int n, int left, int right) {
-  int mid;
-  ll v1, v2, v3;
-  if (left + 1 < right) {
-    mid = (left + right) / 2;
-    v1 = mergeSort(A, n, left, mid);
-    v2 = mergeSort(A, n, mid, right);
-    v3 = merge(A, n, left, mid, right);
-    return v1 + v2 + v3;
-  } else {
-    return 0;
-  }
-}
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
 
 void solve() {
-  int A[MAX], n, i;
-
-  cin >> n;
-  for (i = 0; i < n; i++) cin >> A[i];
-  ll ans = mergeSort(A, n, 0, n);
-  cout << ans << endl;
+  
 }
 
 signed main() {
