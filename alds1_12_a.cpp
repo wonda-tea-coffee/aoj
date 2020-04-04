98c26d785b3955277aa12773631e693b07947a81
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-04 16:41:20 +0900

    解説AC

--- alds1_12_a.cpp
+++ alds1_12_a.cpp
@@ -1,5 +1,3 @@
-// https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/12/ALDS1_12_A
-
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -10,108 +8,107 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
+#include <list>
 #include <map>
 #include <queue>
 #include <set>
 #include <stack>
 #include <string>
+#include <regex>
 #include <vector>
 
-#define fix(n) cout<<fixed<<setprecision(n);
-#define rep(i,n)   for (int i = 0; i < (n); ++i)
-#define sort(a)    sort((a).begin(), (a).end());
-#define uniq(a)    SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end());
-#define reverse(a) reverse((a).begin(), (a).end());
-#define out(d) cout << (d);
-#define outl(d) std::cout<<(d)<<"\n";
-#define Yes() printf("Yes\n");
-#define No() printf("No\n");
-#define YES() printf("YES\n");
-#define NO() printf("NO\n");
-#define ceil(x, y) ((x + y - 1) / (y))
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
 
 using namespace std;
 using ll = long long;
+using ld = long double;
 using P = pair<ll,ll>;
 
 const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
 
-static const int MAX_N = 105;
-int n, costs[MAX_N][MAX_N];
-vector<vector<int>> lists(MAX_N);
+const int MAX = 100;
+const int WHITE = 0;
+const int GRAY = 1;
+const int BLACK = 2;
 
-int mst() {
-  int ret = __INT_MAX__;
+int n, M[MAX][MAX];
 
-  queue<pair<set<int>, int>> q;
-  
+ll prim() {
+  ll u, minv;
+  ll d[MAX], p[MAX], color[MAX];
+
   rep(i, n) {
-    q.push(make_pair(set<int>{i}, 0));
+    d[i] = INF;
+    p[i] = -1;
+    color[i] = WHITE;
   }
 
-  while (!q.empty()) {
-    pair<set<int>, int> v = q.front(); q.pop();
-    set<int> s = v.first;
-    int sum = v.second;
+  d[0] = 0;
 
-    cout << q.size() << endl;
-    cout << "s.size() = " << s.size() << ", sum = " << sum << endl;
-
-    if (s.size() == n) {
-      ret = min(ret, sum);
-      continue;
+  while(1) {
+    minv = INF;
+    u = -1;
+    rep(i, n) {
+      if (d[i] < minv && color[i] != BLACK) {
+        u = i;
+        minv = d[i];
+      }
     }
-
-    for (int i = 0; i < n; i++) {
-      if (s.find(i) != s.end()) continue;
-
-      // cout << "  i = " << i << ", lists[i].size() = " << lists[i].size() << endl;
-
-      for (int j = 0; j < lists[i].size(); j++) {
-        int l = lists[i][j];
-
-        if (s.find(l) == s.end()) continue;
-
-        assert(l != -1);
-
-        if (sum + costs[i][l] < ret) {
-          set<int> ns = s;
-          ns.insert(i);
-
-          assert(costs[i][l] > 0);
-
-          q.push(make_pair(ns, sum + costs[i][l]));
-        }
+    if (u == -1) break;
+    color[u] = BLACK;
+    rep(v, n) {
+      if (color[v] != BLACK && d[v] > M[u][v]) {
+        d[v] = M[u][v];
+        p[v] = u;
+        color[v] = GRAY;
       }
     }
   }
-
-  return ret;
-}
-
-void make_list() {
+  ll sum = 0;
   rep(i, n) {
-    rep(j, n) {
-      if (costs[i][j] != -1) {
-        lists[i].push_back(j);
-        lists[j].push_back(i);
-      }
-    }
+    if (p[i] != -1) sum += M[i][p[i]];
   }
+  return sum;
 }
 
-int main() {
+void solve() {
+  cin >> n;
+  rep(i, n)rep(j, n) {
+    cin >> M[i][j];
+    if (M[i][j] == -1) M[i][j] = INF;
+  }
+
+  outl(prim());
+}
+
+signed main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
   srand((unsigned)time(NULL));
-  fix(12)
+  fix(12);
 
-  cin >> n;
-  rep(i, n)rep(j, n) cin >> costs[i][j];
-
-  make_list();
-
-  outl(int(sizeof(lists)))
-
-  outl(mst())
+  solve();
 }
