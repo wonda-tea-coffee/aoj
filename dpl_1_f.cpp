a45dd8dda5dd5294a187e6e1ebc239f9439b4f0c
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-06 23:24:53 +0900

    solve

---
+++ dpl_1_f.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  ll N, W; cin >> N >> W;
+  vector<ll> v(N), w(N);
+  rep(i, N) cin >> v[i] >> w[i];
+  const ll MAX = 100*100;
+  ll dp[MAX+1];
+  for (int i = 1; i <= MAX; i++) dp[i] = W+1;
+
+  dp[0] = 0;
+  rep(i, N) {
+    for (int j = MAX-v[i]; j >= 0; j--) {
+      if (dp[j] + w[i] <= W) {
+        dp[j+v[i]] = min(dp[j+v[i]], dp[j] + w[i]);
+      }
+    }
+  }
+
+  for (int i = MAX; i >= 0; i--) {
+    if (dp[i] <= W) {
+      outl(i);
+      return;
+    }
+  }
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}
